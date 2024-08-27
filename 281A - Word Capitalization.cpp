4d3f423e317fbba6711bf8f1ea500c4ecac9bbee
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-27 23:06:59 +0530

    Create 281A - Word Capitalization.cpp

---
+++ 281A - Word Capitalization.cpp
@@ -0,0 +1,17 @@
+// 281A - Word Capitalization
+// https://codeforces.com/problemset/problem/281/A
+
+#include <iostream>
+#include <vector>
+#include <cctype>
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+
+    char ch = toupper(str[0]);
+    str[0] = ch;
+    cout << str << endl;
+    return 0;
+}
