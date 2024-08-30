8ab50189b1453479d77fe1de44d952889fbf45ed
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-31 01:12:53 +0530

    Create 104A - Blackjack.cpp

---
+++ 104A - Blackjack.cpp
@@ -0,0 +1,15 @@
+// 104A - Blackjack
+// https://codeforces.com/problemset/problem/104/A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n <= 10 || n > 21) cout << 0 << endl;
+    else if (n == 20) cout << 15 << endl;
+    else cout << 4 << endl;
+    return 0;
+}
