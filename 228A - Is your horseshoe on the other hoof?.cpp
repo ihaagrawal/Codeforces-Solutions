f5006b0ec06016f317f184a961ae0f35691d46ef
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-30 09:14:22 +0530

    Create 228A - Is your horseshoe on the other hoof?.cpp

---
+++ 228A - Is your horseshoe on the other hoof?.cpp
@@ -0,0 +1,24 @@
+// 228A - Is your horseshoe on the other hoof?
+// https://codeforces.com/problemset/problem/228/A
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main(){
+    vector<int> arr(4);
+    for(int i=0;i<4;i++) cin >> arr[i];
+
+    map<int, int> mp;
+    for(int i=0;i<4;i++) mp[arr[i]]++;
+
+    int result = 0;
+    for(auto it : mp){
+        if(it.second > 1) result += it.second - 1;
+    }
+
+    cout << result << endl;
+    return 0;
+}
