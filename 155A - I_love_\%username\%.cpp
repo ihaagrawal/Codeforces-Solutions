6dda2e25e2902d7e6a675378c87128b604b94663
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-31 00:07:27 +0530

    Create 155A - I_love_\%username\%.cpp

---
+++ 155A - I_love_\%username\%.cpp
@@ -0,0 +1,31 @@
+// 155A - I_love_\%username\%
+// https://codeforces.com/problemset/problem/155/A
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    
+    int worst = arr[0], best = arr[0];
+    int count = 0;
+    for(int i=1;i<n;i++){
+        if(arr[i] > best) count++;
+        if(arr[i] < worst) count++;
+        
+        worst = min(worst, arr[i]);
+        best = max(best, arr[i]);
+    }
+
+    cout << count << endl;
+    return 0;
+}
