a3abd6785a5146b50b56c658165a6a69fd42a906
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-30 23:31:59 +0530

    Create 177A2 - Good Matrix Elements.cpp

---
+++ 177A2 - Good Matrix Elements.cpp
@@ -0,0 +1,34 @@
+// 177A2 - Good Matrix Elements
+// https://codeforces.com/problemset/problem/177/A2
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int sum = 0, half = n/2;
+
+    for(int i=0; i<n; i++) sum += arr[i][half];
+    for(int j=0; j<n; j++) sum += arr[half][j];
+    for(int i = 0; i < n; i++){
+        sum += arr[i][i]; 
+        if(i != n-i-1) sum += arr[i][n-i-1]; 
+    }
+    sum -= 2 * arr[half][half];
+
+    cout << sum << endl;
+    return 0;
+}
