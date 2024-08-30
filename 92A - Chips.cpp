5a1cff92e569083061645684b9fa9b605718f2dd
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-31 01:18:18 +0530

    Create 92A - Chips.cpp

---
+++ 92A - Chips.cpp
@@ -0,0 +1,18 @@
+// 92A - Chips
+// https://codeforces.com/problemset/problem/92/A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    m %= (n * (n + 1) / 2);
+    for (int i = 1; i <= n; ++i){
+        if (m < i) break;
+        m -= i;
+    }
+    cout << m << endl;
+    return 0;
+}
