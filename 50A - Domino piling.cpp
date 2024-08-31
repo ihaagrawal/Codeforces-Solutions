53376a0e3f51b6fb871557b053c7d5d942abe316
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-31 08:54:21 +0530

    Create 50A - Domino piling.cpp

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,13 @@
+// 50A - Domino piling
+// https://codeforces.com/problemset/problem/50/A
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int M, N;
+    cin >> M >> N;
+    cout << M * N / 2 << endl;
+    return 0;
+}
