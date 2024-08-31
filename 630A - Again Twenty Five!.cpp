8c1ea398aa206749080a3881fdd5d215a71884dc
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-31 15:49:03 +0530

    Create 630A - Again Twenty Five!.cpp

---
+++ 630A - Again Twenty Five!.cpp
@@ -0,0 +1,12 @@
+// 630A - Again Twenty Five!
+// https://codeforces.com/problemset/problem/630/A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    if (n >= 2) cout << "25" << endl;    
+    return 0;
+}
